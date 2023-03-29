de17a85887f3c980c02bb89fb1a338efd52dbafe
Tirth-021 <125239162+Tirth-021@users.noreply.github.com>
2023-03-29 12:45:07 +0530

    Exception handling added

--- Python_Practicals/Python_Practicals/Practical2.py
+++ Python_Practicals/Python_Practicals/Practical2.py
@@ -9,17 +9,22 @@ def generate_parenthesis(n):
 
 
 try:
-    print("Enter the number n ")
-    n = int(input())
-    if n < 0 or n > 8:
-        raise ValueError
-    if isinstance(n, str):
+     print("Enter the number n ")
+    number = input()
+    if number.isdigit():
+        num = int(number)
+        if num < 0 or num > 8:
+            raise ValueError
+        else:
+            result = generate_parenthesis(int(number))
+            print(result)
+    else:
         raise TypeError
+        
+        
 except ValueError:
     print("Please enter a number between 1 to 8")
 except TypeError:
     print("Please enter a number")
 
-else:
-    result = generate_parenthesis(n)
-    print(result)
+
